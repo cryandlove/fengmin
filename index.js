0dfaedf4115cd497d264ef4d72269964a3fb72fd
cryandlove <109356944+cryandlove@users.noreply.github.com>
2022-07-18 21:03:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $("#login-button").click(function (event) {
   var userName = document.getElementById("userName").value;
   var pwd = document.getElementById("pwd").value;
   //修改密码请改此处
-  if (userName == "丰敏敏" && pwd == "ilovemyself") {
+  if (userName == "丰敏敏" && pwd == "zhunishengrikuaile") {
     event.preventDefault();
     $("form").fadeOut(500);
     $(".wrapper").addClass("form-success");
